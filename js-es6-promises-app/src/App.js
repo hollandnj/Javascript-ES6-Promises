1259dd6f881fd6b2fc887b3bee2d94e7ba72e820
Nick Holland <nick@nicks-air.home>
2018-01-12 16:39:07 +0000

    Returns a list of names using a fetch

---
+++ js-es6-promises-app/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    const url = 'https://randomuser.me/api/?results=20';
+    fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => this.setState({ users: data.results })); 
+  }
+
+  render() {
+    let list = this.state.users.map(user =>{
+      return <div>{user.name.first} {user.name.last}</div>
+    });
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to a list of names</h1>
+        </header>
+        <p className="App-intro">
+          The code is in <code>src/App.js</code>.
+        </p>
+        <div>{list}</div>
+      </div>
+    );
+  }
+}
+
+export default App;
